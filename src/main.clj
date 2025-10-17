285dd8abe38be6969bbeb6cdcaff0b68cc208d4a
thiagopls1 <thiagopls1@hotmail.com>
2025-10-17 12:37:03 -0300

    feat: add `filters` and its implementation

--- src/main.clj
+++ src/main.clj
@@ -1,3 +1,19 @@
-(defn table [tabela] (str "SELECT * FROM ", tabela))
+(defn build_where [acc item]
+    ; Se é o primeiro, não tem AND
+    (if (empty? acc)
+        (str (:field item) " = " (:value item))
+        (str acc " AND " (:field item) " = " (:value item))
+    )
+)
 
+(defn filters [filter_list]
+    (str " WHERE " (reduce build_where "" filter_list))
+)
+
+(defn table [tabela] (str "SELECT * FROM ", tabela))
 (print (table "usuarios"))
+
+(print 
+    (filters [{:field "id", :value 1}]
+    )
+)
