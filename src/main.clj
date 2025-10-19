35d1fd2c6a17ba570b2938039cfa99eb0ce646cd
thiagopls1 <thiagopls1@hotmail.com>
2025-10-19 01:37:49 -0300

    feat: first implementation of `ors`

--- src/main.clj
+++ src/main.clj
@@ -3,7 +3,8 @@
 (def ops {
     :maior ">"
     :menor "<"
-    :menor_igual ">"
+    :menor_igual ">="
+    :maior_igual ">="
     :igual "="
     :in "IN"
     :notin "!IN"
@@ -13,44 +14,61 @@
 ; ------- KV HANDLERS -------
 
 (defn format_by_type [item]
-  (cond
-    (string? item) (str "\"" item "\"")
-    (keyword? item) (str ":" item)
-    (number? item) (str item)
-    (boolean? item) (if item "true" "false")
-    (vector? item) (str "[" (str/join ", " (map format_by_type item)) "]")
-    (list? item) (str "(" (str/join ", " (map format_by_type item)) ")")
-    :else (str item)))
+    (cond
+        (string? item)  (str "\"" item "\"")
+        (keyword? item) (str ":" item)
+        (number? item)  (str item)
+        (boolean? item) (if item "true" "false")
+        (vector? item)  (str "[" (str/join ", " (map format_by_type item)) "]")
+        (list? item)    (str "(" (str/join ", " (map format_by_type item)) ")")
+        :else           (str item))
+)
 
-(defn retrieve_non_kv [[k _]]
+(defn retrieve_non_kv [[k _v]]
     (not= k :field)
 )
 
-(defn handle_item [item]
-    ((first (first 
-        (filter 
-            retrieve_non_kv 
-            (seq item)
-        )
-    )) ops)
+(defn handle_condition [item]
+    (
+        (first (first 
+            (filter 
+                retrieve_non_kv 
+                (seq item)
+            )
+        )) 
+        ops
+    )
 )
 
 (defn handle_value [item]
-    (format_by_type (first (rest (first 
-        (filter 
-            retrieve_non_kv 
-            (seq item)
-        )
-    ))))
+    (format_by_type 
+        (first (rest (first 
+            (filter 
+                retrieve_non_kv 
+                (seq item)
+            )
+        )))
+    )
 )
 
 ; ------- WHERE BUILDER -------
 
+(defn handle_where_set [operator acc item]
+    (if (empty? acc)
+        (str (:field item) " " (handle_condition item) " " (handle_value item))
+        (str acc " " operator " " (:field item) " " (handle_condition item) " " (handle_value item))
+    )   
+)
+
+(defn handle_where_string [operator acc item] 
+    (str acc " " item)
+)
+
 (defn build_where [operator acc item]
     ; Se é o primeiro, não tem "operator"
-    (if (empty? acc)
-        (str (:field item) " " (handle_item item) " " (handle_value item))
-        (str acc " " operator " " (:field item) " " (handle_item item) " " (handle_value item))
+    (cond
+        (string? item) (handle_where_string operator acc item)
+        :else (handle_where_set operator acc item)
     )
 )
 
@@ -66,8 +84,8 @@
 )
 
 ; Aqui vai ter q passar uma função (pra ser usada no lugar do build_where)
-(defn filters [filters_list]
-    (str " WHERE " filters_list)
+(defn filters [filters_query]
+    (str " WHERE " filters_query)
 )
 
 ; ------- FUNÇÕES PRINCIPAIS -------
@@ -96,7 +114,10 @@
                 (ands [
                     { :field "id", :notin [1, 2, 3, 4] },
                     { :field "idade", :maior 25 }
-                    { :field "nome", :diff "eduarda" }
+                    { :field "nome", :in ["eduarda", "josias", "vinicius", "thiago"] }
+                    (ors [
+                        { :field "email" :igual "thiagopls1@homail.com" }
+                    ])
                 ])
             )
         )
